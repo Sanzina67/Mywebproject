ffa7878f4e2ba7fb5e52b4ab1dfa914793d5a9af
Sanzina67 <60129295+Sanzina67@users.noreply.github.com>
2021-09-01 21:56:39 +0600

    Add files via upload

---
+++ admin_login.php
@@ -0,0 +1,60 @@
+<html>
+<head>
+	<title>Admin login</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+	<div class="dropdown">
+       <nav>
+       	<div class="logo">
+       	<img  src="logo.png">
+       	<h4 style="color:white;">Online Libaray Management System</h4>
+       </div>
+
+       	<ul>
+       		<li><a href="index.html">Home</a></li>
+       		<li><a href="#">Login</a>
+       		  <ul>
+       			<li><a href="admin_login.html">Admin login</a></li>
+       			<li><a href="user_login.html">User login</a></li>
+       		  </ul>
+       		</li>
+       		<li><a href="#">Registration</a>
+       		  <ul>
+       			<li><a href="admin_reg.html">Admin registration</a></li>
+       			<li><a href="user_reg.html">User registration</a></li>
+       		  </ul>
+       		</li>
+            <li><a href="books.html">Books</a></li>
+       		<li><a href="#">Feedback</a></li>
+
+       	</ul>
+       </nav>
+       <header>
+              <section>
+                     <div class="login_img">
+                     <br>
+                            <div class="box1">
+                                   <h1 style="text-align: center;font-size: 35px;font-family: Lucida Console;">Library Management System</h1>
+                                   <h1 style="text-align: center;font-size: 25px">Admin Login Form</h1><br>
+                                   <form  action="" method="post">
+                                          <br>
+                                      <div class="login">
+                                          <input type="text" name="Username" placeholder="Username" required=""><br><br>
+                                          <input type="Password" name="Password" placeholder="Password" required=""><br><br>
+                                          <input style="margin:auto 90px; height:25px;width:100px;" type="submit" value="Login">
+                                      </div> 
+                                   </form>
+                                   <p style="color: white; padding-left: 20px;">
+                                          <br><br>
+                                          <a style="color:white;" href="">Forgot Password?</a> &nbsp &nbsp &nbsp
+                                          New to this website?<a style="color:white;" href="admin_reg.html">sign Up</a>
+                                   </p>
+                            </div>
+                     </div>
+              </section>
+       </header>
+</div>
+</body>
+</html>
---
+++ user_reg.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+  <title>Admin login</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+</head>
+<body>
+  <div class="dropdown">
+       <nav>
+        <div class="logo">
+        <img  src="logo.png">
+        <h4 style="color:white;">Online Libaray Management System</h4>
+       </div>
+
+        <ul>
+          <li><a href="index.html">Home</a></li>
+          <li><a href="#">Login</a>
+            <ul>
+            <li><a href="admin_login.html">Admin login</a></li>
+            <li><a href="user_login.html">User login</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Registration</a>
+            <ul>
+              <li><a href="admin_reg.html">Admin registration</a></li>
+              <li><a href="user_reg.html">User registration</a></li>
+            </ul>
+          </li>
+            <li><a href="books.php">Books</a></li>
+          <li><a href="#">Feedback</a></li>
+
+        </ul>
+       </nav>
+       <header>
+              <section>
+                     <div class="login_img">
+                     <br>
+                            <div class="box2">
+                                   <h1 style="text-align: center;font-size: 35px;font-family: Lucida Console;">Library Management System</h1>
+                                   <h1 style="text-align: center;font-size: 25px">User Registration Form</h1><br>
+                                   <form  action="" method="post">
+                                      <div class="login">
+                                          <input style="margin:auto 25px;"type="text" name="first_name" placeholder="First Name" required=""><br><br>
+                                           <input style="margin:auto 25px;"type="text" name="last_name" placeholder="Last name" required=""><br><br>
+                                           <input style="margin:auto 25px;"type="text" name="username" placeholder="Username" required=""><br><br>
+                                    
+                                          <input style="margin:auto 25px;"type="text" name="date_of_birth" placeholder="Date_Of_birth" required=""><br><br>
+                                          <input style="margin:auto 25px;"type="text" name="Email" placeholder="Email" required=""><br><br>
+                                          <input style="margin:auto 25px;"type="Password" name="Password" placeholder="Password" required=""><br><br>
+                                          <input style="margin:auto 110px; height:25px;width:100px;" type="submit" value="Submit">
+                                      </div> 
+                                   </form>
+                                   <p style="color: white; padding-left: 90px;">
+          
+                                         Already have an account?<a style="color:white;" href="user_login.html">Login</a>
+                                   </p>
+                            </div>
+                     </div>
+              </section>
+       </header>
+</div>
+</body>
+</html>
